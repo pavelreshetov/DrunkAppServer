5c942ce9fc731d0e068c6ddba09b262b748fc2f1
Pavel <pavel.snbrd@gmail.com>
2021-01-31 16:44:28 +0300

    - hotfix (response status codes)

--- PackService/views/PackView.py
+++ PackService/views/PackView.py
@@ -7,8 +7,13 @@ class PackView:
 
     def get_packs(self, request):
         if request.method == "GET":
-            try:
-                response = PackAPI().get_all_packs()
-                return JsonResponse(data=response, status=200)
-            except PackModel.DoesNotExist:
+            response = PackAPI().get_all_packs()
+            if not response['data']:
                 return JsonResponse(data={}, status=404)
+            else:
+                return JsonResponse(data=response, status=200)
+        else:
+            response = {
+                "message": "Method not allowed"
+            }
+            return JsonResponse(data=response, status=405)
--- PackService/views/QuestionView.py
+++ PackService/views/QuestionView.py
@@ -7,8 +7,13 @@ class QuestionView:
 
     def get_questions(self, request):
         if request.method == "GET":
-            try:
-                response = QuestionApi().get_all_questions()
-                return JsonResponse(data=response, status=200)
-            except QuestionModel.DoesNotExist:
+            response = QuestionApi().get_all_questions()
+            if not response['data']:
                 return JsonResponse(data={}, status=404)
+            else:
+                return JsonResponse(data=response, status=200)
+        else:
+            response = {
+                "message": "Method not allowed"
+            }
+            return JsonResponse(data=response, status=405)
